075cdfe019a56c38e35368ab822f139f98c16477
Abid Al Wassie <abidalwassie@outlook.com>
2024-08-25 09:46:54 +0600

    upgraded to next-v14

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx", // Ensure the correct path
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  reactStrictMode: true,
+});
