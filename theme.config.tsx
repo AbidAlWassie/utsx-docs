5f131a72386c9a2ff69285a9d94a73dca8a158b7
Abid Al Wassie <abidalwassie@outlook.com>
2024-08-25 22:58:25 +0600

    updated config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,4 +1,5 @@
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>utsx docs</span>,
@@ -12,6 +13,26 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'utsx ui library',
   },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://my-app.com' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'utsx docs'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Documentation for the utsx UI library'}
+        />
+        <title>{frontMatter.title || 'utsx docs'}</title>
+        <meta name="description" content={frontMatter.description || 'Documentation for the utsx UI library'} />
+      </>
+    )
+  }
 }
 
 export default config
