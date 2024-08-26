614ff6b02fffbf56421fdb30535300d35928a967
Abid Al Wassie <abidalwassie@outlook.com>
2024-08-26 12:55:23 +0600

    fix: added route based active navlink + updated docs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React, { useState } from 'react';
 import { FaSearch } from "react-icons/fa";
 
@@ -37,6 +38,8 @@ export const Navbar: React.FC<NavbarProps> = ({
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
+
+  const router = useRouter();
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -144,12 +147,16 @@ export const Navbar: React.FC<NavbarProps> = ({
           } w-full md:flex md:w-auto md:order-1`}
           id="navbar-user"
         >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col font-medium p-4 md:py-0 md:px-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {navItems.map((item, index) => (
               <li key={index}>
                 <a
                   href={item.href}
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={`block py-2 px-3 md:p-0 ${
+                    router.pathname === item.href
+                      ? 'text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500'
+                      : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                  }`}
                 >
                   {item.label}
                 </a>
