beaf4e7b1e8c1c8e196f0f08752da53a91bde3e2
Abid Al Wassie <abidalwassie@outlook.com>
2024-08-26 10:47:36 +0600

    updated Navbar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,8 @@
 export { Accordion } from "./Accordion";
 export { Cards } from "./Cards";
 export { FilterButtons } from "./FilterButtons";
+export { Navbar } from "./Navbar";
+export { Navbar1 } from "./Navbar1";
+// export { Navbar3 } from "./Navbar3";
 export { PersonCard } from "./PersonCard";
 
